5ab0b2c22dc241e06fa18fc31fd49677b6ff3f0e
Louis Ventre <louis.ventre@epitech.eu>
2017-12-02 18:24:35 -0800

    Add script to generate VS Solution

---
+++ GenerateVSSolution.bat
@@ -0,0 +1,6 @@
+cmake -G "Visual Studio 15"
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x86
+devenv LibNetworks.sln /build Release
+move lib\Release\cubznetworks.lib ..\bin
+move lib\Release\proto.lib ..\bin
+move lib_src\proto\TCPMessage.pb.h ..\include
