b613f2e1b8abcfcbdaf4c9cbb2102d292a79d5f6
Louis Ventre <louis.ventre@epitech.eu>
2017-12-18 16:19:12 -0800

    Update script, fix test client for Windows

--- scripts/windows/GenerateVSSolution.bat
+++ scripts/windows/GenerateVSSolution.bat
@@ -1,4 +1,8 @@
 cd ..\..
+
+copy scripts\linux\ProtoCMakeLists.txt lib_src\proto\CMakeLists.txt
+
+
 cmake -G "Visual Studio 15"
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x86
 devenv LibNetworks.sln /build Release
