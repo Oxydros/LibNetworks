e1097cee345db5fcc07860fc33c2cc81d5571e4b
Louis Ventre <louis.ventre@epitech.eu>
2017-12-22 21:40:12 -0800

    Add UDP client / server unit test

---
+++ scripts/linux/testTCP.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+outputWanted=`cat -e tests/outputTCP.txt`
+
+cd ./build
+
+##Launching server in background
+./bin/tcpServer &> outputServer &
+serverPID=$!
+
+echo "Launched server on $serverPID"
+
+echo "Waiting 5secs";
+sleep 5;
+
+./bin/tcpClient &> outputClient &
+clientPID=$!
+
+echo "Launched client on $clientPID"
+
+echo "Waiting 5secs";
+sleep 5;
+
+kill -s "SIGTERM" $clientPID;
+sleep 1;
+kill -s "SIGTERM" $serverPID;
+
+echo "Client and Server terminated, comparing responses"
+
+response=$(cat -e outputServer | grep "Received type");
+
+if [ "$response" == "$outputWanted" ]
+then
+    rm outputServer;
+    rm outputClient;
+    echo "Success!";
+    exit 0;
+fi
+
+echo "Failed!";
+
+echo "=========================";
+echo "Output SERVER";
+echo "=========================";
+cat outputServer;
+echo "=========================";
+echo "Output CLIENT";
+echo "=========================";
+cat outputClient;
+echo "=========================";
+
+rm outputServer;
+rm outputClient;
+
+exit 42;
+
